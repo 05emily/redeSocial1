f4c5802692f72d95d18aad51ccbcb01087f219f5
Emily <emilyferr0511@gmail.com>
2026-09-09 11:47:40 -0300

    ok

--- scrript.js
+++ scrript.js
@@ -19,8 +19,9 @@ document.addEventListener("DOMContentLoaded", () => (
 
     function formatLikes (num){
         if(num >=1000){
-            
+           return (num/1000) .toFixed(1)+"K";
         }
+        return num.toString();
     }
 
 
