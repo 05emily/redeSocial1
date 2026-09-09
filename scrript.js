d636755481ed20c55e5d129ba0c18665cda902ab
Emily <emilyferr0511@gmail.com>
2026-09-09 11:31:14 -0300

    ok

---
+++ scrript.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => (
+    const likeBtn = document.querySelector(".like-btn");
+    const postMedia = document.querySelector(".post-media");
+    if (!likeBtn) return;
+
+    const LikesCountSpan = likeBtn.querySelector(".likes-count");
+    const bookmarkBtn = document.querySelector(".bookmark-btn");
+
+    let isLiked = false;
+    let baseLikes = 0;
+
+    
+))
